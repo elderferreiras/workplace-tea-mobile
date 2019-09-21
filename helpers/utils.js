28088fc6e1919dad2439262357329c0959ed598e
Elder Patten Ferreira <elderferreirass@gmail.com>
2019-09-21 16:24:31 -0600

    fixe dissue with locale not working on android

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,12 +1,35 @@
+import { Platform } from 'react-native';
+
 export const getDate = (date, short) => {
     const objDate = new Date(date);
-    let month = objDate.toLocaleString('default', { month: 'long' });
-    return `${month} ${objDate.getDate()}, ${objDate.getFullYear()}`
+    const day = objDate.getDate();
+    const month = getMonth(objDate.getMonth() + 1);
+    const year = objDate.getFullYear();
+    return `${month} ${day}, ${year}`
+};
+
+export const getMonth = (month) => {
+  switch (month) {
+      case 1: return "January";
+      case 2: return "February";
+      case 3: return "March";
+      case 4: return "April";
+      case 5: return "May";
+      case 6: return "June";
+      case 7: return "July";
+      case 8: return "August";
+      case 9: return "September";
+      case 10: return "October";
+      case 11: return "November";
+      case 12: return "December";
+      default: return "";
+  }
 };
 
 export const getShortDate = (date, short) => {
     const objDate = new Date(date);
     let month = objDate.toLocaleString('default', { month: 'long' });
+
     return `${month.toString().slice(0,3)} ${objDate.getDate()}, ${objDate.getFullYear()}`
 };
 
