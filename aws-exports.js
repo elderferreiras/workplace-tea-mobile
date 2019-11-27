bd388c715d5cd827e6c279d249b777d6f5c4ca4c
Elder Patten Ferreira <elderferreirass@gmail.com>
2019-11-26 19:46:46 -0700

    change workplace id

--- aws-exports.js
+++ aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://2lw5kwwgofdtzcuvnopc5pmg4u.appsync-api.us-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-jzbeypahgbdojba3u7jolcrmvu"
+    "aws_appsync_apiKey": "da2-l53lbbyo7jbe7eaewqs4apwona"
 };
 
 
